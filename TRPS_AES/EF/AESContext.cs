447ecdd94147ec0916b5a7040491825ff5fdbbaa
MavSiya <mav.programmer@gmail.com>
2023-11-24 14:53:41 +0200

    added repositories

--- TRPS_AES/EF/AESContext.cs
+++ TRPS_AES/EF/AESContext.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TRPS_AES.Entities;
 
 namespace TRPS_AES.EF
 {
-    internal class AESContext
+     class AESContext : DbContext
     {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Cards> Cards { get; set; }
+        public DbSet<Reports> Reports { get; set; }
+
+        public AESContext(DbContextOptions<AESContext> options) : base(options)
+        {
+        }
+
     }
 }
